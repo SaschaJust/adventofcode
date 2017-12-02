6678c94b54a4c51474a8d17463cbd8fc53aa621b
Sascha Just <sascha.just@outlook.com>
2017-12-02 15:48:29 -0500

    Adding one pipeline solution for star_02 part 2 in elixir.

--- star_02/star_02.ex
+++ star_02/star_02.ex
@@ -15,3 +15,20 @@ input \
   |> Enum.reduce({0, -1}, fn(x, {sum, sign}) -> {sum + sign * x, -sign} end) \
   |> elem(0)) end) \
 |> IO.puts
+# result: 48357
+
+input \
+|> String.trim \
+|> String.split("\n") \
+|> Enum.reduce(0, fn(x, acc_x) -> acc_x +
+  (x \
+  |> String.split("\t") \
+  |> Enum.map(fn(x) -> String.to_integer(x) end) \
+  |> (&Enum.reduce(&1, {nil, nil}, fn(x, {divisor, divider}) ->
+    if divider == nil do {x, Enum.find(&1, fn(y) -> x != y and rem(x, y) == 0 end)} else {divisor, divider} end
+   end)).() \
+  |> Tuple.to_list \
+  |> Enum.reduce(0, fn(x, result) -> if result == 0 do x else div(result, x) end end))
+end) \
+|> IO.puts
+# result: 351
