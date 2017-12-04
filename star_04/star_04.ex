46a849dbb2a026f3d191854ff8b684aa10795d26
Sascha Just <sascha.just@outlook.com>
2017-12-04 11:23:03 -0500

    Adding 1 pipeline solution to star 04 in elixir.

---
+++ star_04/star_04.ex
@@ -0,0 +1,25 @@
+{_, input} = File.read("star_04.input")
+
+input \
+|> String.trim \
+|> String.split("\n") \
+|> Enum.filter(fn(x) ->
+  (x \
+  |> String.split(" ") \
+  |> (&Enum.count(&1) == (&1 |> Enum.uniq |> Enum.count)).()
+  ) end)
+|> Enum.count
+|> IO.puts
+# result: 383
+
+input \
+|> String.trim \
+|> String.split("\n") \
+|> Enum.filter(fn(x) ->
+  (x \
+  |> String.split(" ") \
+  |> Enum.map(fn(x) -> x |> String.graphemes |> Enum.sort end)
+  |> (&Enum.count(&1) == (&1 |> Enum.uniq |> Enum.count)).()
+  ) end)
+|> Enum.count
+|> IO.puts
