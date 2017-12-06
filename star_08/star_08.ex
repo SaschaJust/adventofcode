0f1b38a8e1b5fd32c1f5bf3f7f72acd710def78d
Sascha Just <sascha.just@outlook.com>
2017-12-06 12:08:13 -0500

    Adding elixir solution to day 8 puzzle.

---
+++ star_08/star_08.ex
@@ -0,0 +1,43 @@
+{_, input} = File.read("star_08.input")
+
+map_list = input \
+  |> String.trim \
+  |> String.split("\n") \
+  |> Enum.map(fn x -> ~r/(?<name>\S+) (?<instruction>inc|dec) (?<delta>-?\d+) if (?<lhs>\w+) (?<op>>|<|>=|<=|!=|==) (?<rhs>-?\d+)/ \
+    |> Regex.named_captures(x) end)
+
+defmodule Star08 do
+  def fun(operator) do
+    case operator do
+      "<=" -> &<=/2
+      "<" -> &</2
+      ">=" -> &>=/2
+      ">" -> &>/2
+      "==" -> &==/2
+      "!=" -> &!=/2
+      "inc" -> &(&1+&2)
+      "dec" -> &(&1-&2)
+    end
+  end
+
+  def process([ %{"name" => name, "instruction" => instruction, "delta" => delta, "lhs" => lhs, "op" => op, "rhs" => rhs} | remainder ], registers, max) do
+    if Star08.fun(op).(
+      Map.get(registers, lhs, 0),
+      String.to_integer(rhs)
+    ) do
+      new_value = Star08.fun(instruction).(
+        Map.get(registers, name, 0),
+        String.to_integer(delta)
+      );
+      process(remainder, Map.put(registers, name, new_value), max(max, new_value))
+    else
+      process(remainder, registers, max)
+    end
+  end
+
+  def process([], registers, max) do
+    {registers |> Map.values |> Enum.max, max}
+  end
+end
+
+map_list |> Star08.process(Map.new(), 0) |> IO.inspect
